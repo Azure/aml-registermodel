22fdfee5db90c500a42e88bb253b9b8bc2ae771d
Ashish Kumar (ASHKUMA) <ashishonce@gmail.com>
2020-04-29 17:54:02 +0530

    adding support for local model registration

--- code/main.py
+++ code/main.py
@@ -1,7 +1,7 @@
 import os
 import json
 
-from azureml.core import Workspace, Experiment, Run
+from azureml.core import Workspace, Experiment, Run, Model
 from azureml.core.authentication import ServicePrincipalAuthentication
 from azureml.core.resource_configuration import ResourceConfiguration
 from azureml.exceptions import AuthenticationException, ProjectSystemException, UserErrorException, ModelPathNotFoundException, WebserviceException
@@ -77,62 +77,74 @@ def main():
         print(f"::error::Workspace authorization failed: {exception}")
         raise ProjectSystemException
 
-    # Loading experiment
-    print("::debug::Loading experiment")
-    try:
-        experiment = Experiment(
-            workspace=ws,
-            name=experiment_name
-        )
-    except UserErrorException as exception:
-        print(f"::error::Loading experiment failed: {exception}")
-        raise AMLConfigurationException(f"Could not load experiment. Please your experiment name as input parameter.")
+    # check if user has provided model path
+    model_path = None;
+    from_local = False
 
-    # Loading run by run id
-    print("::debug::Loading run by run id")
-    try:
-        run = Run(
-            experiment=experiment,
-            run_id=run_id
-        )
-    except KeyError as exception:
-        print(f"::error::Loading run failed: {exception}")
-        raise AMLConfigurationException(f"Could not load run. Please your run id as input parameter.")
-
-    # Loading best run
-    print("::debug::Loading best run")
-    best_run = get_best_run(
-        experiment=experiment,
-        run=run,
-        pipeline_child_run_name=parameters.get("pipeline_child_run_name", "model_training")
-    )
-
-    # Comparing metrics of runs
-    print("::debug::Comparing metrics of runs")
     # Default model name
     repository_name = os.environ.get("GITHUB_REPOSITORY").split("/")[-1]
     branch_name = os.environ.get("GITHUB_REF").split("/")[-1]
     default_model_name = f"{repository_name}-{branch_name}"
-    if not parameters.get("force_registration", False):
-        compare_metrics(
-            workspace=ws,
-            run=best_run,
-            model_name=parameters.get("model_name", default_model_name)[:32],
-            metrics_max=parameters.get("metrics_max", []),
-            metrics_min=parameters.get("metrics_min", [])
+
+    # if user has provided run_id , we respect it. 
+    if run_id is not None:
+        
+        # Loading experiment
+        print("::debug::Loading experiment")
+        try:
+            experiment = Experiment(
+                workspace=ws,
+                name=experiment_name
+            )
+        except UserErrorException as exception:
+            print(f"::error::Loading experiment failed: {exception}")
+            raise AMLConfigurationException(f"Could not load experiment. Please your experiment name as input parameter.")
+
+        # Loading run by run id
+        print("::debug::Loading run by run id")
+        try:
+            run = Run(
+                experiment=experiment,
+                run_id=run_id
+            )
+        except KeyError as exception:
+            print(f"::error::Loading run failed: {exception}")
+            raise AMLConfigurationException(f"Could not load run. Please add your run id as input parameter.")
+
+        # Loading best run
+        print("::debug::Loading best run")
+        best_run = get_best_run(
+            experiment=experiment,
+            run=run,
+            pipeline_child_run_name=parameters.get("pipeline_child_run_name", "model_training")
         )
+
+        # Comparing metrics of runs
+        print("::debug::Comparing metrics of runs")
+        if not parameters.get("force_registration", False):
+            compare_metrics(
+                workspace=ws,
+                run=best_run,
+                model_name=parameters.get("model_name", default_model_name)[:32],
+                metrics_max=parameters.get("metrics_max", []),
+                metrics_min=parameters.get("metrics_min", [])
+            )
+
+        # Defining model path
+        print("::debug::Defining model path")
+        model_file_name = parameters.get("model_file_name", "model.pkl")
+        model_file_name = os.path.split(model_file_name)[-1]
+        # finally update the model path
+        model_path = [file_name for file_name in best_run.get_file_names() if model_file_name in os.path.split(file_name)[-1]][0]
+    else:
+        model_path  = parameters.get("model_file_name", "model.pkl") # if run id is not present then check if model file is present locally, in that case the path is defined
+        from_local = True
 
     # Defining model framework
     print("::debug::Defining model framework")
     model_framework = get_model_framework(
         name=parameters.get("model_framework", None)
     )
-
-    # Defining model path
-    print("::debug::Defining model path")
-    model_file_name = parameters.get("model_file_name", "model.pkl")
-    model_file_name = os.path.split(model_file_name)[-1]
-    model_path = [file_name for file_name in best_run.get_file_names() if model_file_name in os.path.split(file_name)[-1]][0]
 
     # Defining datasets
     print("::debug::Defining datasets")
@@ -158,27 +170,48 @@ def main():
     cpu = parameters.get("cpu", None)
     memory = parameters.get("memory", None)
     resource_configuration = ResourceConfiguration(cpu=cpu, memory_in_gb=memory) if (cpu is not None and memory is not None) else None
-
-    try:
-        model = best_run.register_model(
-            model_name=parameters.get("model_name", default_model_name)[:32],
-            model_path=model_path,
-            tags=parameters.get("model_tags", None),
-            properties=parameters.get("model_properties", None),
-            model_framework=model_framework,
-            model_framework_version=parameters.get("model_framework_version", None),
-            description=parameters.get("model_description", None),
-            datasets=datasets,
-            sample_input_dataset=input_dataset,
-            sample_output_dataset=output_dataset,
-            resource_configuration=resource_configuration
-        )
-    except ModelPathNotFoundException as exception:
-        print(f"::error::Model name not found in outputs folder. Please provide the correct model file name and make sure that the model was saved by the run: {exception}")
-        raise AMLConfigurationException(f"Model name not found in outputs folder. Please provide the correct model file name and make sure that the model was saved by the run.")
-    except WebserviceException as exception:
-        print(f"::error::Model could not be registered: {exception}")
-        raise AMLConfigurationException("Model could not be registered")
+    if from_local is True:
+        try:
+            model = Model.register(workspace=ws,
+                        model_name=parameters.get("model_name", default_model_name)[:32],
+                        model_path=model_path,
+                        tags=parameters.get("model_tags", None),
+                        properties=parameters.get("model_properties", None),
+                        model_framework=model_framework,
+                        model_framework_version=parameters.get("model_framework_version", None),
+                        description=parameters.get("model_description", None),
+                        datasets=datasets,
+                        sample_input_dataset=input_dataset,
+                        sample_output_dataset=output_dataset,
+                        resource_configuration=resource_configuration
+                    )
+        except UserErrorException as exception:
+            print(f"::error::Model not found at the mentioned path : {exception}")
+            raise AMLConfigurationException(f"Model file not found in path. Please provide the correct model file name with path and make sure that the model exist in the repository.")
+        except WebserviceException as exception:
+            print(f"::error::Model could not be registered: {exception}")
+            raise AMLConfigurationException("Model could not be registered")
+    else:
+        try:
+            model = best_run.register_model(
+                model_name=parameters.get("model_name", default_model_name)[:32],
+                model_path=model_path,
+                tags=parameters.get("model_tags", None),
+                properties=parameters.get("model_properties", None),
+                model_framework=model_framework,
+                model_framework_version=parameters.get("model_framework_version", None),
+                description=parameters.get("model_description", None),
+                datasets=datasets,
+                sample_input_dataset=input_dataset,
+                sample_output_dataset=output_dataset,
+                resource_configuration=resource_configuration
+            )
+        except ModelPathNotFoundException as exception:
+            print(f"::error::Model name not found in outputs folder. Please provide the correct model file name and make sure that the model was saved by the run: {exception}")
+            raise AMLConfigurationException(f"Model name not found in outputs folder. Please provide the correct model file name and make sure that the model was saved by the run.")
+        except WebserviceException as exception:
+            print(f"::error::Model could not be registered: {exception}")
+            raise AMLConfigurationException("Model could not be registered")
 
     # Create outputs
     print("::debug::Creating outputs")
